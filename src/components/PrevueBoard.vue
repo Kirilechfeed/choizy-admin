7a6e7b33ffe461cf79e52c96f61d35448d80a989
Can4itos <90899407+Kirilechfeed@users.noreply.github.com>
2022-11-29 20:12:47 +0300

    ready

--- src/components/PrevueBoard.vue
+++ src/components/PrevueBoard.vue
@@ -1,8 +1,39 @@
 <template>
-  <div class="">hello</div>
+  <div class="container-preview">
+    <div class="container-main">
+      <div class="preview-img">
+        <img alt="preview" :src="require('@/assets/img/preview/preview.svg')" />
+      </div>
+      <div class="preview-title">
+        Welcom to the Choizy dashboard statistic overview will be displayed here
+        soon.
+      </div>
+    </div>
+  </div>
 </template>
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 @Component
 export default class PrevueBoard extends Vue {}
 </script>
+<style>
+.container-preview {
+  padding-top: 155px;
+}
+.container-main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.preview-img {
+}
+.preview-title {
+  width: 381px;
+  font-size: 20px;
+  line-height: 27px;
+  text-align: center;
+  color: #7788ae;
+  margin-top: 45px;
+}
+</style>
